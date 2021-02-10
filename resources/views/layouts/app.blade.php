872805758509470d2320c9c63f591340973a5d47
zaxwebs <zifidigital@gmail.com>
2021-02-10 06:36:26 +0530

    added coreui

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CoreUI CSS -->
+	<link rel="stylesheet" href="https://unpkg.com/@coreui/icons@2.0.0-beta.3/css/all.min.css">
+	<link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+	<title>CoreUI</title>
+</head>
+
+<body class="c-app">
+	@include('layouts.partials.sidebar')
+	<div class="c-wrapper">
+		<header class="c-header">
+			<!-- Header content here -->
+			<button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar"
+				data-class="c-sidebar-lg-show" responsive="true">
+				<i class="cil-hamburger-menu"></i>
+			</button>
+		</header>
+		<div class="c-body">
+			<main class="c-main">
+				@yield('content')
+			</main>
+		</div>
+		<footer class="c-footer">
+			<!-- Footer content here -->
+			&copy; Zack Webster
+		</footer>
+	</div>
+
+	<!-- CoreUI JavaScript -->
+	<script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.bundle.min.js"></script>
+</body>
+
+</html>
