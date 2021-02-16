cf47734aa0a103e2336d72ce8fa0a5adc2bab88b
zaxwebs <zifidigital@gmail.com>
2021-02-16 13:02:27 +0530

    added remove product action

--- app/Http/Livewire/OrderCreateForm.php
+++ app/Http/Livewire/OrderCreateForm.php
@@ -11,10 +11,13 @@ class OrderCreateForm extends Component
     public $orderProducts;
     public $newProduct = null;
 
+    public $oldOrderProducts;
+
     public $productSkeleton = [
         'id' => null,
         'unit' => null,
         'quantity' => 0,
+        'rate' => 0,
     ];
 
     public function mount() {
@@ -22,17 +25,30 @@ class OrderCreateForm extends Component
         $this->orderProducts = [ $this->productSkeleton ];
     }
 
+    public function updatingOrderProducts() {
+        $this->oldOrderProducts = $this->orderProducts;
+    }
+
     public function updatedOrderProducts() {
+        $oldIds = array_column($this->oldOrderProducts, 'id');
+        $ids = array_column($this->orderProducts, 'id');
+
         foreach($this->orderProducts as $index => $orderProduct) {
             if($orderProduct['id'] !== null) {
                 $product = $this->allProducts->firstWhere('id', $orderProduct['id']);
                 $this->orderProducts[$index]['unit'] = $product->unit->name;
+                $this->orderProducts[$index]['rate'] = $product->selling_price;
             }
         }
     }
 
     public function addProduct() {
         $this->orderProducts[] = $this->productSkeleton;
+    }
+
+    public function removeProduct($index) {
+        unset($this->orderProducts[$index]);
+        $this->orderProducts = array_values($this->orderProducts);
     }
 
     public function render() {
--- resources/views/livewire/order-create-form.blade.php
+++ resources/views/livewire/order-create-form.blade.php
@@ -7,8 +7,8 @@
                 <pre class="col-sm-12">{{ json_encode($orderProducts) }}</pre>
             </div>
             @foreach ($orderProducts as $index => $orderProduct)
-            <div class="row">
-                <div class="col-md-3">
+            <div wire:key="{{ $index }}" class="row">
+                <div class="col-lg-3">
                     <div class="form-group">
                         <label for="">Select Product</label>
                         <select wire:model="orderProducts.{{ $index }}.id" class="form-control" id="">
@@ -19,19 +19,36 @@
                         </select>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-lg-3">
                     <div class="form-group">
                         <label for="">Quantity</label>
                         <div class="input-group">
-
                             <input wire:model.lazy="orderProducts.{{ $index }}.quantity" class="form-control"
-                                id="cost_price" name="cost_price" type="number" min="0" step="any"
-                                value="{{ $orderProduct['quantity'] }}">
+                                type="number" min="0" step="any" value="{{ $orderProduct['quantity'] }}">
                             <div class="input-group-append">
                                 <div class="input-group-text">
                                     {{ $orderProduct['unit'] ? $orderProduct['unit'] : 'Unit' }}</div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="form-group">
+                        <label for="">Rate per {{ $orderProduct['unit'] ? $orderProduct['unit'] : 'Unit' }}</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">$</div>
+                            </div>
+                            <input wire:model.lazy="orderProducts.{{ $index }}.rate" class="form-control" type="number"
+                                min="0" step="any" value="{{ $orderProduct['rate'] }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="form-group">
+                        <label class="d-block" for="">Actions</label>
+                        <button class="btn btn-primary" wire:click.prevent="removeProduct({{ $index }})">Remove
+                            Product</button>
                     </div>
                 </div>
             </div>
