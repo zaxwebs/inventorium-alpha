adc2fe0ff9068dffcaa9e1906a86dbcb5bd9048e
zaxwebs <zifidigital@gmail.com>
2021-02-17 15:54:29 +0530

    removed debugger

--- resources/views/livewire/order-create-form.blade.php
+++ resources/views/livewire/order-create-form.blade.php
@@ -3,7 +3,6 @@
         @csrf
         <div class="card-header"><strong>Add an Order</strong></div>
         <div class="card-body">
-            <pre>{{ var_dump($productRates) }}</pre>
             <div class="row">
                 <div class="col-sm-12">
                     <div class="form-group">
